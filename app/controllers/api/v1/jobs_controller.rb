b9bbdbdf3c3a3ebec4bd44c7ee75a0db9ca4f90e
projectmaking123 <jason.89.wang@gmail.com>
2017-08-09 21:36:40 -0400

    added show method and tested with postman

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -5,6 +5,11 @@ module Api
         jobs = Job.order('created_at DESC')
         render json: {status: 'Success', message: 'loaded jobs', data:jobs}, status: :ok
       end
+
+      def show
+        job = Job.find_by(id: params[:id])
+        render json: {status: 'Success', message: 'loaded jobs', data:job}, status: :ok
+      end
     end
   end
 end
