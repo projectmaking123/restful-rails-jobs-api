5f878420158dbce7a2d11a636fe8b8aea0577c91
projectmaking123 <jason.89.wang@gmail.com>
2017-08-09 21:34:31 -0400

    mod jobs controller, changed v1 to V1, made job pural, tested index route with postman

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,9 +1,9 @@
 module Api
-  module v1
-    class JobController < ApplicationController
+  module V1
+    class JobsController < ApplicationController
       def index
         jobs = Job.order('created_at DESC')
-        render json: {status: 'Success', message: 'loaded jobs', data: jobs}, status: ok
+        render json: {status: 'Success', message: 'loaded jobs', data:jobs}, status: :ok
       end
     end
   end
