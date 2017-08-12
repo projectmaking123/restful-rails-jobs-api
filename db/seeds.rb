e9b17537d7a31aaa31ef5317b83aa70a8e70e993
projectmaking123 <jason.89.wang@gmail.com>
2017-08-12 19:07:15 -0400

    re-did seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 
+User.create(email: 'test@gmail.com', password: 'password', password_confirmation: 'password')
 
 10.times do
   Job.create({
     title: Faker::Job.title,
     field: Faker::Job.field,
     key_skill: Faker::Job.key_skill,
-    description: Faker::Lorem.paragraph(2, true)
+    description: Faker::Lorem.paragraph(2, true),
+    user_id: 1
     })
 end
-
-User.create(email: 'test@gmail.com', password: 'password', password_confirmation: 'password')
