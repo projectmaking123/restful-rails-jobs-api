af6762345560a5a92b9ce3ed35d47dfe0c8285cc
projectmaking123 <jason.89.wang@gmail.com>
2017-08-16 23:11:39 -0400

    mod seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
     description: Faker::Lorem.paragraphs(sentence_count=8, 2),
     contact: Faker::Internet.email,
     location: Faker::Address.city,
-    uid: "97p839qnR9go0LI0lvL5hPoRozt2"
+    uid: "Em2bv95Q2eMI3IHRANcV1VRQllx1"
   })
 end
