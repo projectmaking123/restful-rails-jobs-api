8a1296da5ab4a802ee9c8ec7a28701064f365c58
projectmaking123 <jason.89.wang@gmail.com>
2017-08-17 23:14:21 -0400

    is able to create applicant on rails side

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,13 @@ module Api
   module V1
     class UsersController < ApplicationController
       def create
+        job = Job.find_by(id: params[:job_id])
         @user = User.find_or_create_by!(uid: params[:user][:uid]) do |user|
           user.email = params[:user][:email]
           user.name = params[:user][:name]
         end
         if @user
+          job.applicants.push(@user)
           render :create
         else
           head(:unprocessable_entity)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      resources :jobs
-      resources :users, only: [:create]
+      resources :jobs do
+        resources :users, only: [:create]
+      end
     end
   end
 end
