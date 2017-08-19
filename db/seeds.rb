59d04092d919e86325df8e6caf230f0668404129
projectmaking123 <jason.89.wang@gmail.com>
2017-08-18 21:26:37 -0400

    mod seed location to 48 wall street new york ny

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
     key_skill: Faker::Job.key_skill,
     description: Faker::Lorem.paragraphs(sentence_count=8, 2),
     contact: Faker::Internet.email,
-    location: "New York City",
+    location: "48 wall street new york ny",
     uid: "Em2bv95Q2eMI3IHRANcV1VRQllx1"
   })
 end
